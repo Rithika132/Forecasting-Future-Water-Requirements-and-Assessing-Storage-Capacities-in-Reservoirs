44b9502da8d281966f79552639efbb8d11c3df5a
Rithika132 <rithikaveeramani667@gmail.com>
2026-07-19 13:58:53 +0530

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,11 +1,12 @@
 function forecast(population, demandPerPerson, years) {
-    const growthRate = 0.02;
+    const growthRate = 0.02; // 2% population growth
+    
     let futurePopulation = population * Math.pow((1 + growthRate), years);
-    let waterDemand = futurePopulation * demandPerPerson;
-
+    let estimatedDemand = futurePopulation * demandPerPerson;
+    
     return {
         futurePopulation: Math.round(futurePopulation),
-        waterDemand: Math.round(waterDemand)
+        estimatedDemand: Math.round(estimatedDemand)
     };
 }
 
